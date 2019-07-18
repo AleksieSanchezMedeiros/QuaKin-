eac96a25e92da17aeafce4450abeae68289d2243
ScythePhantom <aleksieisboss@gmail.com>
2019-07-18 09:23:53 -0700

    wip

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+  
+    public void LoadLevel()
+    {
+int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        LoadLevel();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
